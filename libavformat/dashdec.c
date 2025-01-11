7d9252eb017394577a10209c624d92123658f99c
skyjet18 <57456827+skyjet18@users.noreply.github.com>
2025-01-11 17:27:27 +0100

    dashdec: use min buffer time when selecting start offset for live stream

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -1426,7 +1426,14 @@ static int64_t calc_cur_seg_no(AVFormatContext *s, struct representation *pls)
             num = pls->first_seq_no;
         } else if (pls->n_timelines) {
             av_log(s, AV_LOG_TRACE, "in n_timelines mode\n");
-            start_time_offset = get_segment_start_time_based_on_timeline(pls, 0xFFFFFFFF) - 60 * pls->fragment_timescale; // 60 seconds before end
+            if (c->min_buffer_time > 0)
+            {
+                start_time_offset = get_segment_start_time_based_on_timeline(pls, 0xFFFFFFFF) - (2 * c->min_buffer_time) * pls->fragment_timescale;
+            }
+            else
+            {
+                start_time_offset = get_segment_start_time_based_on_timeline(pls, 0xFFFFFFFF) - 60 * pls->fragment_timescale; // 60 seconds before end
+            }
             num = calc_next_seg_no_from_timelines(pls, start_time_offset);
             if (num == -1)
                 num = pls->first_seq_no;
